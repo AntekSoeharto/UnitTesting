30c032fc4498724e6bca18ff0b04c79baa1f880f
valentinus140201 <81425238+valentinus140201@users.noreply.github.com>
2022-03-18 14:42:57 +0700

    PasienTester

--- src/test/java/PasienTester.java
+++ src/test/java/PasienTester.java
@@ -32,14 +32,14 @@ public class PasienTester {
         ArrayList<Dokter> listDokterActual = condok.getAllDokter();
 //        Assertions.assertThat(dokters.getNID()).isEqualTo(temp.getNID());
 
-        for (int i = 0; i < listDokterActual.size(); i++) {
-            Assertions.assertEquals(listDokterExpected.get(i).getNID(), listDokterActual.get(i).getNID());
+        for (int i = 0; i < listPasienActual.size(); i++) {
+            Assertions.assertEquals(listPasienExpected.get(i).getNIK(), listPasienActual.get(i).getCabang());
         }
     }
 
     @Test
     public void testAddPasienTrue(){
-        Pasien pasien = new Pasien("5", "Dokter Spesialis Anak", "Dave", "258025", new Date(2020), "A", "Pria", "TKO", "05850585");
+        Pasien pasien = new Pasien("10", "10", new Date(2020), "+B", "Pria", "10", "10", "NON_BPJS", "10", "10");
         boolean masuk = condok.insertNewPasien(pasien);
         boolean sukses = true;
         Assertions.assertEquals(sukses, masuk);
